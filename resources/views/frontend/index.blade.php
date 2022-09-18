846d7b45da42399d23e24c3ec68860986fb94e42
Kantai <kantai.developer@gmail.com>
2022-09-18 22:38:18 +0800

    移除首頁多餘的資訊

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -75,11 +75,9 @@
              * @property {array} strings strings to be typed
              */
             strings: [
-                '『大象呢，你的大象呢』\n『把你的大象找出來』\n『算了先看我的大象』\n『我們一起看喔』\n...'
-                , '一個測試工程師走進一家酒吧，要了一杯啤酒\n一個測試工程師走進一家酒吧，要了一杯咖啡\n一個測試工程師走進一家酒吧，要了 999999999 杯啤酒\n一個測試工程師走進一家酒吧，要了 0 杯啤酒\n一個測試工程師走進一家酒吧，要了 -1 杯啤酒，\n...'
-                , '這案子已經完成了 90%，\n接下來就交給你收尾了。'
-                , 'PHP 睡太久了\nPHP 這二、三十年來\nPHP 沒有好好稱霸開發者生態\n偉大的 PHP 這個巨人\n要清醒囉！'
-                , '大佬：「我大佬」\n學霸：「我學霸」\n神仙：「我神仙」\n天才：「我天才」\n裝弱的電神：「我弱」\n大佬、電神、學霸、神仙、天才：「我弱」\n...'
+                '媽的勒，學姊很討厭欸，一直亂電人，自己也沒有多厲害 ...',
+                '幹，我的防疫獎金去哪裡了 ...',
+                '護理師公會還存在做什麼啊？要不要廢掉？'
             ],
 
             /**
@@ -142,32 +140,6 @@
             <div class="w-100 text-center">
                 <p class="pt-2 my-0">我沒有想寫懶加載的意思，所以給一個文章列表的連結，你們自己去看吧😎👍</p>
                 <a class="btn btn-bg btn-lg h1 py-2 px-5 my-2" href="{{ route('frontend.social.cards.index') }}">@lang('Posts List')</a>
-            </div>
-            <!--more-->
-
-            <hr class="border margin-y-md">
-
-            <h1 class="pb-2 mb-2">最新收到的投稿👇</h1>
-            @foreach($newCards as $card)
-                    <div class="media my-2 pb-2">
-                        <img class="rounded mr-2 thumb gallery-slideshow" style="width: 128px; height: 128px" src="{{ $card->getPicture() }}" alt="#{{ appName() . base_convert($card->id, 10, 36) }}" />
-                        <div class="media-body pt-2">
-                            <a class="text-decoration-none" style="color: var(--font-primary-color) !important;" href="{{ route('frontend.social.cards.show', ['id' => $card->id]) }}">
-                                <div style="display: flow-root">
-                                    <h4 class="float-left my-0">#{{ appName() . base_convert($card->id, 10, 36) }}</h4>
-                                    <p class="float-right my-0">@displayDate($card->updated_at, 'Y/m/d h:s:i') ({{ $card->updated_at->diffForHumans() }})</p>
-                                </div>
-                                <p class="mb-0">{{ $card->getContent(200) }}</p>
-                            </a>
-                        </div>
-                    </div>
-            @endforeach
-
-            <hr class="border margin-y-md">
-
-            <div class="w-100 text-center">
-                <p class="pt-2 my-0">我沒有想寫懶加載的意思，所以給一個群眾審核的連結，你們自己去看吧😎👍</p>
-                <a class="btn btn-bg btn-lg h1 py-2 px-5 my-2" href="{{ route('frontend.social.cards.review') }}">@lang('Review Submit')</a>
             </div>
             <!--more-->
         </div>
