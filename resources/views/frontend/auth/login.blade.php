13910aba5ee87675a4c51d1f8d90edc0e468a177
Kantai <kantai.developer@gmail.com>
2022-09-19 16:39:00 +0800

    Update login.blade.php

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -54,4 +54,31 @@
         </div><!--overlay-container-->
     </div><!--container-->
 
+    <div id="container" class="container d-block d-md-none" style="width: 90%; min-height: 640px;">
+        <div class="form-container" style="width: 100%; left: 0px;">
+            <x-forms.post :action="route('frontend.auth.login')" class="px-3">
+                <h1>{{ __('Login') }}</h1>
+                <div class="social-container py-2">
+                    @include('frontend.auth.includes.social')
+                </div>
+                <span class="py-2">{{ __('or use your account') }}</span>
+                <div class="form-group row mb-2 w-100">
+                    <label for="email" class="col-md-12 col-form-label text-md-right pb-1">@lang('E-mail Address')</label>
+
+                    <div class="col-md-12">
+                        <input type="email" name="email" id="email" class="form-control form-control-lg" placeholder="{{ __('E-mail Address') }}" value="{{ old('email') }}" maxlength="255" required autofocus autocomplete="email" />
+                    </div>
+                </div><!--form-group-->
+                <div class="form-group row mb-2 w-100">
+                    <label for="password" class="col-md-12 col-form-label text-md-right pb-1">@lang('Password')</label>
+
+                    <div class="col-md-12">
+                        <input type="password" name="password" id="password" class="form-control form-control-lg" placeholder="{{ __('Password') }}" maxlength="255" required autofocus autocomplete="email" />
+                    </div>
+                </div><!--form-group-->
+                <x-utils.link :href="route('frontend.auth.password.request')" class="btn btn-link" :text="__('Forgot Your Password?')" />
+                <button class="btn btn-dos btn-lg btn-block my-4" type="submit">{{ __('Login') }}</button>
+            </x-forms.post>
+        </div><!--sign-in-container-->
+    </div><!--container-->
 @endsection
