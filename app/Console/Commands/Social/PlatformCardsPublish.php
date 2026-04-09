f935ba1756578262d1f2eaa3dcf3f762ca206efb
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <Kantai235@users.noreply.github.com>
2026-04-09 22:14:41 +0800

    Update PlatformCardsPublish.php

--- app/Console/Commands/Social/PlatformCardsPublish.php
+++ app/Console/Commands/Social/PlatformCardsPublish.php
@@ -75,7 +75,7 @@ class PlatformCardsPublish extends Command
      *
      * @var int
      */
-    protected $delayMinutes = 60;
+    protected $delayMinutes = 30;
 
     /**
      * Execute the console command.
